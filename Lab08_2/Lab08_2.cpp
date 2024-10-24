5c42bdceda9e4e41e8ba6b0fdc1d9c34b855a5ec
Tylko-jedno <diogkek@gmail.com>
2024-10-24 22:25:10 +0300

    first master commit

---
+++ Lab08_2/Lab08_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+size_t MaxWordLength(const string& s) {
+    size_t tmp = 0;
+    size_t maxLength = 0;
+    for (char c : s) {
+        if (c != ' ')
+            tmp++;
+        else {
+            if (tmp > maxLength) {
+                maxLength = tmp;
+            }
+            tmp = 0;
+        }
+    }
+
+    if (tmp > maxLength) {
+        maxLength = tmp;
+    }
+
+    return maxLength;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    size_t res = MaxWordLength(str);
+    cout << "Length of the longest word: " << res << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"   // Для Microsoft Unit Testing Framework
+#include "CppUnitTest.h"
+#include <string>
+#include "../Lab08_2/Lab08_2.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(MaxWordLengthTest)
+    {
+    public:
+
+        // Тест для рядка з кількома словами
+        TEST_METHOD(TestMultipleWords)
+        {
+            string input = "This is a test string";
+            size_t expected = 6;  // Найдовше слово - "string"
+            Assert::AreEqual(expected, MaxWordLength(input));
+        }
+    };
+}
